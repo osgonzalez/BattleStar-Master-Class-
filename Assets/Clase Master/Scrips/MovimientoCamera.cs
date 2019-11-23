11a4e504a7213232c9aeb7262f1e04eb8e9238ec
Oscar Servia Gonzalez <osgonzalez@esei.uvigo.es>
2019-11-23 20:59:49 +0100

    Add scene

---
+++ Assets/Clase Master/Scrips/MovimientoCamera.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoCamera : MonoBehaviour {
+
+	private GameObject player;
+
+	private Vector3 distancia;
+
+	void Start()
+	{
+		//Obtemenos el gameObject con el tag Player
+		player=GameObject.FindGameObjectWithTag("Player");
+		//Obtenemos la distancia a la que se halla originalmente la camara del jugador
+		distancia = transform.position - player.transform.position;
+	}
+
+	void LateUpdate()
+	{
+		//Actualizamos la posicion de la camara
+		transform.position = player.transform.position + distancia;
+	}
+}
---
+++ Assets/Clase Master/Scrips/MovimientoJugador.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoJugador : MonoBehaviour {
+
+	private Rigidbody rb;
+	public float speed=1;
+	public float rotateSpeed = 1;
+
+
+
+	// Use this for initialization
+	void Start () {
+		this.rb = this.GetComponent<Rigidbody>();
+		//rb.freezeRotation = true;
+		//rb.useGravity = false;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		//Recoje el valor del imput horizontal y vertical y devielve un valor entre [-1,1]
+		float movHor= Input.GetAxis("Horizontal");
+		float movVer = Input.GetAxis("Vertical");
+
+		//rb.AddForce((new Vector3(movHor, 0, movVer)) * speed * Time.deltaTime);
+		this.rb.velocity =(new Vector3(movHor, 0, movVer)) * speed ;
+
+
+		if (Input.GetKey("l"))
+		{
+			rb.rotation = Quaternion.Euler((rb.rotation.eulerAngles + new Vector3(0, rotateSpeed, 0)));
+		}
+
+		if (Input.GetKey("k"))
+		{
+			rb.rotation = Quaternion.Euler((rb.rotation.eulerAngles + new Vector3(0, -rotateSpeed, 0)));
+		}
+
+	}
+}
